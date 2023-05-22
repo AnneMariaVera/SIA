391f4c4121785b10fa545e80d25e6d40bb50f04c
AnneMariaVera <anne.hermann@student.uni-tuebingen.de>
2023-05-22 11:25:57 +0200

    added functions for boundary conditions

--- SIA.py
+++ SIA.py
@@ -12,7 +12,7 @@ Overview:
                 2. the bed is constant along the x axis.
         
 Input variables: 
-    base        Location of the base of the ice sheet
+    bed         Location of the bed of the ice sheet
     surface     Location of the surface of the ice sheet 
     boundary    contains information about the flux at the boundary
     a           Accumultaion rate
@@ -28,51 +28,168 @@ Input variables:
 
 import numpy as np
 
-
-def flux(base, surface, boundary, delta_x, delta_t, n, rho, A, g):
-    H = surface-base    # ice thickness
-    Q = 2*A/(n+2)*(rho*g)**n*np.array(
-        [((surface[i+1]-surface[i])/delta_x)**n*((H[i+1]+H[i])/2)**(n+2)
-         for i in range(0, len(surface)-1)])
-    # Append the flux at the boundaries
-    Q = np.append(boundary[0], Q)
-    Q = np.append(Q, boundary[1])
-    return Q
-
-
-def velocity(base, surface, boundary, delta_x, n, rho, A, g):
+def velocity_00(bed, surface, boundary, delta_x, n, rho, A, g):
     # Calculate the mean velocity
-    # the velocity at the base is zero
-    H = surface-base    # ice thickness
+    # the velocity at the bed is zero
+    H = surface-bed    # ice thickness
     v_x = -2*A/(n+2)*(rho*g)**n*np.array(
         [((surface[i+1]-surface[i])/delta_x)**n*H[i]**(n+1)
             for i in range(0, len(surface)-1)])
     return v_x
 
+def velocity_11(bed, surface, boundary, delta_x, n, rho, A, g):
+    # Calculate the mean velocity
+    # the velocity at the bed is zero
+    H = surface-bed    # ice thickness
+    v_x = -2*A/(n+2)*(rho*g)**n*np.array(
+        [((surface[i+1]-surface[i])/delta_x)**n*H[i]**(n+1)
+            for i in range(0, len(surface)-1)])
+    if H[len(H)-1]!=0:
+        v_x = np.append(v_x,boundary[1,0]/H[len(H)-1])
+    return v_x
 
-def height(base, surface, boundary, a, delta_x, delta_t, n, rho, A, g):
-    Q = flux(base, surface, boundary, delta_x, delta_t, n, rho, A, g)
+def velocity_01(bed, surface, boundary, delta_x, n, rho, A, g):
+    # Calculate the mean velocity
+    # the velocity at the bed is zero
+    H = surface-bed    # ice thickness
+    v_x = -2*A/(n+2)*(rho*g)**n*np.array(
+        [((surface[i+1]-surface[i])/delta_x)**n*H[i]**(n+1)
+            for i in range(0, len(surface)-1)])
+    if H[len(H)-1]!=0:
+        v_x = np.append(v_x,boundary[1,0]/H[len(H)-1])
+    return v_x
+
+def velocity_10(bed, surface, boundary, delta_x, n, rho, A, g):
+    # Calculate the mean velocity
+    # the velocity at the bed is zero
+    H = surface-bed    # ice thickness
+    v_x = -2*A/(n+2)*(rho*g)**n*np.array(
+        [((surface[i]-surface[i-1])/delta_x)**n*H[i]**(n+1)
+            for i in range(1, len(surface))])
+    if H[0]!=0:
+        v_x = np.append(boundary[0,0]/H[0],v_x)
+    return v_x
+
+def flux_11(bed, surface, boundary, delta_x, delta_t, n, rho, A, g):
+    H = surface-bed    # ice thickness
+    Q = 2*A/(n+2)*(rho*g)**n*np.array(
+        [((surface[i+1]-surface[i])/delta_x)**n*((H[i+1]+H[i])/2)**(n+2)
+         for i in range(0, len(surface)-1)])
+    # Append the flux at the boundaries
+    Q = np.append(boundary[0,1], Q)
+    Q = np.append(Q, boundary[1,1])
+    return Q
+
+def flux_10(bed, surface, boundary, delta_x, delta_t, n, rho, A, g):
+    H = surface-bed    # ice thickness
+    Q = 2*A/(n+2)*(rho*g)**n*np.array(
+        [((surface[i+1]-surface[i])/delta_x)**n*((H[i+1]+H[i])/2)**(n+2)
+         for i in range(0, len(surface)-1)])
+    # Append the flux at the boundaries
+    Q = np.append(boundary[0,1], Q)
+    return Q
+
+def flux_01(bed, surface, boundary, delta_x, delta_t, n, rho, A, g):
+    H = surface-bed    # ice thickness
+    Q = 2*A/(n+2)*(rho*g)**n*np.array(
+        [((surface[i+1]-surface[i])/delta_x)**n*((H[i+1]+H[i])/2)**(n+2)
+         for i in range(0, len(surface)-1)])
+    # Append the flux at the boundaries
+    Q = np.append(Q, boundary[1,1])
+    return Q
+
+def flux_00(bed, surface, boundary, delta_x, delta_t, n, rho, A, g):
+    H = surface-bed    # ice thickness
+    Q = 2*A/(n+2)*(rho*g)**n*np.array(
+        [((surface[i+1]-surface[i])/delta_x)**n*((H[i+1]+H[i])/2)**(n+2)
+         for i in range(0, len(surface)-1)])
+    return Q
+
+# def velocity_inner(bed, surface, delta_x,nz, n, rho, A, g):
+#     # TODO: Think about grid in ice sheet (bottom to top)
+#     # Pattyn
+#     # Calculate the inner velocity
+#     # the velocity at the base is zero
+#     H = surface-bed    # ice thickness
+#     v_inner = np.array([np.linspace(bed[i],surface[i],nz)])
+#     v_x = -2*A/(n+2)*(rho*g)**n*np.array(
+#         [((surface[i+1]-surface[i])/delta_x)**n*H[i]**(n+1)
+#             for i in range(0, len(surface)-1)])
+#     return v_x
+
+def height_11(bed, surface, boundary, a, delta_x, delta_t, n, rho, A, g):
+    Q = flux_11(bed, surface, boundary, delta_x, delta_t, n, rho, A, g)
     h_new = np.array([surface[i]+delta_t/delta_x*(Q[i+1]-Q[i])
                       +a*delta_t for i in range(0, len(Q)-1)])
-    h_new = [base[i] if h_new[i]-base[i] < 0 else h_new[i]
-             for i in range(0, len(base))]
+    h_new = [bed[i] if h_new[i]-bed[i] < 0 else h_new[i]
+             for i in range(0, len(bed))]
+    return h_new
+
+def height_10(bed, surface, boundary, a, delta_x, delta_t, n, rho, A, g):
+    Q = flux_10(bed, surface, boundary, delta_x, delta_t, n, rho, A, g)
+    h_new = np.array([surface[i]+delta_t/delta_x*(Q[i+1]-Q[i])
+                      +a*delta_t for i in range(0, len(surface)-1)])
+    h_new = np.append(h_new,boundary[1,1])
+    h_new = [bed[i] if h_new[i]-bed[i] < 0 else h_new[i]
+             for i in range(0, len(bed))]
+    return h_new
+
+def height_01(bed, surface, boundary, a, delta_x, delta_t, n, rho, A, g):
+    Q = flux_01(bed, surface, boundary, delta_x, delta_t, n, rho, A, g)
+    h_new = np.array([surface[i]+delta_t/delta_x*(Q[i]-Q[i-1])
+                      +a*delta_t for i in range(1, len(surface)-1)])
+    h_new = np.append(boundary[0,1],h_new)
+    h_new = [bed[i] if h_new[i]-bed[i] < 0 else h_new[i]
+             for i in range(0, len(bed))]
+    return h_new
+
+def height_00(bed, surface, boundary, a, delta_x, delta_t, n, rho, A, g):
+    Q = flux_00(bed, surface, boundary, delta_x, delta_t, n, rho, A, g)
+    h_new = np.array([surface[i]+delta_t/delta_x*(Q[i]-Q[i-1])
+                      +a*delta_t for i in range(1, len(surface)-1)])
+    h_new = np.append(boundary[0,1],h_new)
+    h_new = np.append(h_new,boundary[1,1])
+    h_new = [bed[i] if h_new[i]-bed[i] < 0 else h_new[i]
+             for i in range(0, len(bed))]
     return h_new
 
 
-def solution(base, surface, boundary, a, delta_x, delta_t, t_0, N, n, rho, A, g):
+def solution(bed, surface, boundary, a, delta_x, delta_t, t_0, N, n, rho, A, g):
+    if (boundary[0,0],boundary[1,0])==(0,0):
+        flux = flux_00
+        height = height_00
+        velocity=velocity_00
+    elif (boundary[0,0],boundary[1,0])==(0,1):
+        flux = flux_01
+        height = height_01
+        velocity = velocity_01
+    elif (boundary[0,0],boundary[1,0])==(1,0):
+        flux = flux_10
+        height = height_10
+        velocity = velocity_10
+    elif (boundary[0,0],boundary[1,0])==(1,1):
+        flux = flux_11
+        height = height_11
+        velocity = velocity_11
+
+
     # save initial condition in solution list sol_h, sol_v and Q
     sol_h = [surface]
-    sol_v = [velocity(base, surface, boundary, delta_x, n, rho, A, g)]
-    Q = [flux(base, surface, boundary, delta_x, delta_t, n, rho, A, g)]
+    sol_v = [velocity(bed, surface, boundary, delta_x, n, rho, A, g)]
+    Q = [flux(bed, surface, boundary, delta_x, delta_t, n, rho, A, g)]
     timesteps = 1
+    sol_h.append(height(bed, sol_h[len(sol_h)-1], boundary, a, delta_x, delta_t, n, rho, A, g))
+    sol_v.append(velocity(bed, sol_h[len(sol_h)-1], boundary ,delta_x, n, rho, A, g))
+    Q.append(flux(bed, sol_h[len(sol_h)-1],boundary, delta_x, delta_t, n, rho, A, g))
+    timesteps = timesteps+1
     # Iterate through time and append solution to sol list
-    while timesteps <= N:
+    while timesteps <= N and max(abs(np.array(sol_h[len(sol_h)-1])-np.array(sol_h[len(sol_h)-2])))>=0.00001:
         sol_h.append(
-            height(base, sol_h[len(sol_h)-1], boundary, a, delta_x, delta_t, n, rho, A, g))
+            height(bed, sol_h[len(sol_h)-1], boundary, a, delta_x, delta_t, n, rho, A, g))
         sol_v.append(
-            velocity(base, sol_h[len(sol_h)-1], boundary, delta_x, n, rho, A, g))
-        Q.append(flux(base, sol_h[len(sol_h)-1],
+            velocity(bed, sol_h[len(sol_h)-1], boundary ,delta_x, n, rho, A, g))
+        Q.append(flux(bed, sol_h[len(sol_h)-1],
                  boundary, delta_x, delta_t, n, rho, A, g))
         timesteps = timesteps+1
 
-    return [sol_h, sol_v, Q]
+    return [sol_h, sol_v, Q,timesteps]
